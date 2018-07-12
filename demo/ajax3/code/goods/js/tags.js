4dd6edd5d7ab35b369c4e6b35ca1401c2c142da0
your_username <your_registered_github_Email>
2018-07-12 14:11:35 +0800

    anli

--- demo/ajax3/code/goods/js/tags.js
+++ demo/ajax3/code/goods/js/tags.js
@@ -1,13 +1,23 @@
 //注册鼠标悬停事件
-console.log(1);
-// var pan = $('goodslist span');
-// console.log(pan);
-// 获取单签标签中的文本
-// 发送ajax请求
-// 通过$.post 向后端发送ajax请求;
-// 接收返回的结果
-// 通过模板引擎将结果显示在页面上
-// 1. 引入引擎库文件
-// 2. 设置样板并拼接 页面上需要的结构
-// 3. 调用模板引擎方法将样板和后台返回的数据融合
-// 4. 将融合好的数据添加到页面中
+var pan = $('.goodslist span').mouseenter(function () {
+    // 获取单签标签中的文本
+    var key = $(this).html()
+    // 发送ajax请求
+    // 通过$.post 向后端发送ajax请求;
+    $.post('getGoodslist.php', {"keyword": key}, function (amg) {
+            // 接收返回的结果
+            // 通过模板引擎将结果显示在页面上
+            // 1. 引入引擎库文件
+            // 2. 设置样板并拼接 页面上需要的结构
+            // 3. 调用模板引擎方法将样板和后台返回的数据融合
+            var str = template('tpl', amg)
+            // 4. 将融合好的数据添加到页面中
+            $('.goodslist_wrap div')[0].append(str);
+            console.log($('.goodslist_wrap  div')[0]);
+            
+    })
+})
+
+
+
+
